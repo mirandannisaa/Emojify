1ddb39e1b6df56f5d04a27f712d79f62f40c5df1
mohamed <moh4med.ahmed@gmail.com>
2017-12-26 16:50:08 +0200

    make main layout.

--- app/src/main/java/com/example/compucity/emojify/MainActivity.java
+++ app/src/main/java/com/example/compucity/emojify/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.compucity.emojify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView mImageView;
+    private Button goButton;
+    private FloatingActionButton msharefab;
+    private FloatingActionButton msavefab;
+    private FloatingActionButton mclearfab;
+    private TextView titleTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView=(ImageView)findViewById(R.id.image_view);
+        goButton=(Button)findViewById(R.id.emojify_button);
+        msharefab=(FloatingActionButton)findViewById(R.id.share_button);
+        mclearfab=(FloatingActionButton)findViewById(R.id.clear_button);
+        msavefab=(FloatingActionButton)findViewById(R.id.save_button);
     }
 }
